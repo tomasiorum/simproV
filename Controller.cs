45f217730035368f0292defe0853bbc75b34201c
700926 <700926UniversidadeAberta>
2023-05-05 20:33:45 +0100

    Alterar .Core para EntityFrameWork 4.6.1

--- Controller.cs
+++ Controller.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimproV
 {
-    class Controller{
+    public class Controller
+    {
         View view;
         Model model;
 
--- Model.cs
+++ Model.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SimproV
 {
-    class Model {
+    class Model
+    {
         private Controller controller;
         private View view;
         private ListaFaturas listapendentes;
